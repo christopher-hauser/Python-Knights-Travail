1a7cc5232f884c3aa23c988a86846cc93c85059c
Tommy Forde <tommyforde@Tommys-MacBook-Pro.local>
2021-12-16 16:46:22 -0600

    did some work

--- tree.py
+++ tree.py
@@ -13,13 +13,14 @@ class Node:
         return self._children
 
     def add_child(self, node):
-        self._children.append(node)
-        if (not node in self._children):
+        if (node not in self._children):
+            self._children.append(node)
             node.parent = self
 
     def remove_child(self, node):
-        self._children.remove(node)
-        node.parent = None
+        if node in self.children:
+            self._children.remove(node)
+            node.parent = None
 
     @property
     def parent(self):
@@ -27,5 +28,22 @@ class Node:
 
     @parent.setter
     def parent(self, node):
+        if len(self._children) and self._parent != None:
+            print('here')
+            self._parent.remove_child(self)
+
         self._parent = node
-        node.add_child(self)
+
+        if (node and self not in node.children):
+            node.add_child(self)
+
+
+node1 = Node('root1')
+node2 = Node('root2')
+node3 = Node('root3')
+
+node3.parent = node1
+node1.remove_child(node3)
+
+print('_______________________________________')
+
